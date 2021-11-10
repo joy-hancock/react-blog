00633033e05795da6f394c301f4c74d2911e371f
joy_h <joy@designjoy.org>
2021-11-10 17:03:21 -0500

    Finished lesson 7 with react events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 //Homepage content gets its own component, to nest under root component.  Therefore it gets its own file.//
 
 const Home = () => {
-    return ( 
+  //Function for event handler of button.//
+  const handleClick = (e) => {
+  console.log("hello, friends", e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  }
+  return ( 
         //classes being applied for using CSS later.
-      <div className="home"></div>
+      <div className="home">
+        <h2>Homepage</h2>
+        {/*Dynamic event listener with jsx, passing function handleClick
+        You can't invoke function immediately, because the values will be passed to the console without clicking.*/}
+        <button onClick={handleClick}>Click me</button>
+        =
+      </div>
      );
 }
  
